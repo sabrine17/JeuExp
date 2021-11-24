b1493e912f2bd63d029c8fe9ced8878c31c265c7
EmileParent <parentcoe@gmail.com>
2021-11-24 11:07:55 -0500

    Gitignore et avancements connexion

--- Assets/Scripts/ConnexionServeur.cs
+++ Assets/Scripts/ConnexionServeur.cs
@@ -13,6 +13,15 @@ public class ConnexionServeur : MonoBehaviourPunCallbacks
 
     public GameObject ecranSalle;
     public GameObject ecranNom;
+    public GameObject ecranChoixPerso;
+
+    bool j1Selectione;
+    bool j2Selectione;
+
+    public GameObject imgLockJ1;
+    public GameObject imgLockJ2;
+    public GameObject txtJoueurConnectes;
+
 
     private void Awake()
     {
@@ -21,13 +30,13 @@ public class ConnexionServeur : MonoBehaviourPunCallbacks
 
     public override void OnConnectedToMaster()
     {
-        print("Connecté " + PhotonNetwork.IsConnected);
+        print("Connectï¿½ " + PhotonNetwork.IsConnected);
         PhotonNetwork.JoinLobby();
     }
 
     public override void OnDisconnected(DisconnectCause cause)
     {
-        print("le joueur a déconnecté " + cause.ToString());
+        print("le joueur a dï¿½connectï¿½ " + cause.ToString());
     }
 
     public override void OnJoinedLobby()
@@ -45,6 +54,17 @@ public class ConnexionServeur : MonoBehaviourPunCallbacks
     {
         print(PhotonNetwork.CurrentRoom.Name);
         ecranSalle.SetActive(false);
+        ecranChoixPerso.SetActive(true);
+        
+        if (PhotonNetwork.LocalPlayer.IsMasterClient == true) {
+            j1Selectione = true;
+        }
+
+        else {
+            j2Selectione = true;
+        }
+
+        print(j1Selectione);
     }
 
     // Start is called before the first frame update
@@ -56,7 +76,43 @@ public class ConnexionServeur : MonoBehaviourPunCallbacks
     // Update is called once per frame
     void Update()
     {
-        
+        if (j1Selectione) {
+            imgLockJ1.SetActive(true);
+        }
+
+        else {
+            imgLockJ1.SetActive(false);
+        }
+
+        if (j2Selectione) {
+            imgLockJ2.SetActive(true);
+        }
+
+        else {
+            imgLockJ2.SetActive(false);
+        }
+
+        if (ecranChoixPerso.activeSelf) {
+            txtJoueurConnectes.GetComponent<Text>().text = "Joueurs connectÃ©s " + PhotonNetwork.CurrentRoom.PlayerCount + " / " + PhotonNetwork.CurrentRoom.MaxPlayers;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (ecranChoixPerso.activeSelf)
+            {
+                PhotonNetwork.LeaveRoom();
+                ecranChoixPerso.SetActive(false);
+                ecranSalle.SetActive(true);
+            }
+
+            else if (ecranSalle.activeSelf)
+            {
+                PhotonNetwork.LeaveLobby();
+                ecranSalle.SetActive(false);
+                ecranNom.SetActive(true);
+            }
+        }
+
     }
 
     public void CreerSalle()
@@ -64,7 +120,7 @@ public class ConnexionServeur : MonoBehaviourPunCallbacks
         RoomOptions roomOptions = new RoomOptions();
         roomOptions.MaxPlayers = 2;
         PhotonNetwork.CreateRoom(inputNomSalle.GetComponent<InputField>().text, roomOptions, null);
-    } 
+    }
 
     public void JoindreSalle()
     {
@@ -78,4 +134,16 @@ public class ConnexionServeur : MonoBehaviourPunCallbacks
         ecranSalle.SetActive(true);
         print(PhotonNetwork.LocalPlayer.NickName);
     }
+
+   /* public void SelectionJ1() {
+        if (ptSelectionnerJ1) {
+            j1Selectione = true;
+        }
+    }
+
+    public void SelectionJ2() {
+        if (ptSelectionnerJ2) {
+            j2Selectione = true;
+        }
+    }*/
 }
