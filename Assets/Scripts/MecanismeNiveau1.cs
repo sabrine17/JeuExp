62ae3e3aee5a66a7828daf18a7cfc70acf7b45f5
manexonmorin <62068818+manexonmorin@users.noreply.github.com>
2021-12-08 11:14:48 -0500

    niveau 1 intégration minimale

--- Assets/Scripts/MecanismeNiveau1.cs
+++ Assets/Scripts/MecanismeNiveau1.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
 
-public class MecanismeNiveau1 : MonoBehaviour
+public class MecanismeNiveau1 : MonoBehaviourPunCallbacks
 {
-    private GameObject perso;
+    public Sprite porteOuvert; // sprite de la porte qui est ouvert
+    public Sprite porteFermer; // Sprite de la porte qui est fermer
+
+    public GameObject porte; // variable pour selectionner la porte;
+
+    private bool mort; // variable pour vérifier la mort
+
+    public Text lesLumieres;
+    private int nombreDeLumieres = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,15 +26,69 @@ public class MecanismeNiveau1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        lesLumieres.text = "Torches : " + nombreDeLumieres + "/2";
+
+        // vérifier si le personnage meurt -- François
+        if (mort == true)
+        {
+            // Activation de l'animation de mort --François
+            GetComponent<Animator>().SetBool("mort", true);
+
+            // Désactiver le collider -- François
+            GetComponent<BoxCollider2D>().enabled = false;
+
+            // Empecher que le personnage bouge quand il est mort -- François
+            GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+
+            // desactiver le scale qui se truve dans le script mouvement perso pour pas qu'il se tourne -- François
+            GetComponent<MouvementPerso>().transform.localScale = new Vector3(1f, transform.localScale.y, transform.localScale.z);
+        }
 
     }
 
     void OnCollisionEnter2D(Collision2D colision)
     {
+        // vérifier si le personnage touche l'objet avec le tag danger (ce sont les pics) -- François
         if (colision.gameObject.tag == "Danger")
         {
-            GetComponent<Collider2D>().enabled = false;
-            print("Collider s'efface");
+            // mettre la variable booléene mort à true -- François
+            mort = true;
+
+            // Change la scène après 3 secondes
+            Invoke("ChangementDeScene", 3f);
         }
+        else
+        {
+            // si le personnage n'a pas toucher au pics alors il n'est pas mort --François
+            mort = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // vérifier si le personnage entre en collision avec la porte
+        if(collision.gameObject.name == "door_4")
+        {
+            // changer le sprite de la porte --François
+            porte.GetComponent<SpriteRenderer>().sprite = porteOuvert;
+        }
+        else
+        {
+            // Si il ne touche pas la porte, alors il se ferme -- François
+            porte.GetComponent<SpriteRenderer>().sprite = porteFermer;
+        }
+
+        if(collision.gameObject.tag == "feu")
+        {
+            nombreDeLumieres += 1 ;
+            lesLumieres.GetComponent<Text>().text = nombreDeLumieres.ToString();
+            collision.gameObject.SetActive(false);
+        }
+    }
+
+    void ChangementDeScene()
+    {
+        // Recharge la scene pour tout les joueurs -- François
+        PhotonNetwork.LoadLevel(SceneManager.GetActiveScene().name);
     }
 }
